109df0d9ea41079974966666360e610b5f39c091
Belousovaao <belousovaao@mail.ru>
2026-02-22 20:45:30 +0700

    fixed database problems and added migration with constraint

--- RentFlow.Api/obj/Debug/net9.0/RentFlow.Api.AssemblyInfo.cs
+++ RentFlow.Api/obj/Debug/net9.0/RentFlow.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e71089eef30e7fdf0a9c0dfb77327f3d753d673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f8d7627b7f58df0798e038442e716525c85d44")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RentFlow.Persistance/obj/Debug/net9.0/RentFlow.Persistance.AssemblyInfo.cs
+++ RentFlow.Persistance/obj/Debug/net9.0/RentFlow.Persistance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentFlow.Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e71089eef30e7fdf0a9c0dfb77327f3d753d673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f8d7627b7f58df0798e038442e716525c85d44")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentFlow.Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentFlow.Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
